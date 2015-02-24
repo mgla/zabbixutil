0dfa8ee2a06471db2be0a77fbed196f6b309df15
Maik Glatki <maik.glatki@gmail.com>
2015-02-24 17:33:00 +0100

    New Item: SSH password auth allowed.t

---
+++ zabbix.debian.ssh.passwordauthentication/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Check if debian file for reboot notificatione exists.
+# Author: Maik Glatki <maik.glatki@gmail.com>, 2014
+
+SCRIPT=debian.ssh.passwordauthentication
+ZABBIXCONF=/etc/zabbix/zabbix_agentd.conf
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Check if item is installed.
+COUNT=$(grep "$SCRIPT" $ZABBIXCONF| wc -l)
+if [ $COUNT -eq 0 ]; then
+	echo "Installing item: $SCRIPT."
+	cat $DIR/UserParameter >> $ZABBIXCONF
+	service zabbix-agent restart
+else
+	echo "Item already installed: $SCRIPT."
+fi
+
